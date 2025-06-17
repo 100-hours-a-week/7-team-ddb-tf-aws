8565c3ae98a2b9fcd41795035e37763dc662055a
Dongseok Kang <dsk08208@gmail.com>
2025-06-17 16:40:52 +0900

    Feat: AWS 환경 초기 설정  (#8)
    - terraform backend 구축
    - envs(dev,prod,shared,static)  설정
     #1

---
+++ backend/backend.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_version = "1.11.4"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.99.1"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "terraform_state" { 
+  bucket = "dolpin-terraform-state-bn2gz7v3he1rj0ia"
+  force_destroy = false
+}
+
+resource "aws_s3_bucket_versioning" "enabled" { 
+  bucket = aws_s3_bucket.terraform_state.id 
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" { 
+  bucket = aws_s3_bucket.terraform_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.terraform_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
---
+++ envs/dev/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "1.11.4"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.99.1"
+    }
+  }
+  backend "s3" {
+    bucket       = "dolpin-terraform-state-bn2gz7v3he1rj0ia"
+    key          = "dev/terraform/terraform.tfstate"
+    region       = "ap-northeast-2"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
+# AWS Provider
+provider "aws" {
+  region = var.aws_region
+}
