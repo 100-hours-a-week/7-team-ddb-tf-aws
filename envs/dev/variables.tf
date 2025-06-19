946d7701a9d8f3136b9103c7b7044261ee87c32a
Jiyoung Shin <sjy140801@dgu.ac.kr>
2025-06-19 16:26:09 +0900

    Feat: dev 환경에 fe, be ecr 구성 및 기본 변수 추가
     #5

--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -62,3 +62,34 @@ variable "env" {
   type        = string
   default     = "dev"
 }
+
+### ECR
+variable "be_ecr_name" {
+  description = "Backend ECR 리포지토리 이름"
+  type        = string
+  default     = "dolpin-backend"
+}
+
+variable "fe_ecr_name" {
+  description = "Frontend ECR 리포지토리 이름"
+  type        = string
+  default     = "dolpin-frontend"
+}
+
+variable "image_tag_mutability" {
+  description = "태그 변경 가능 여부"
+  type        = string
+  default     = "IMMUTABLE"
+}
+
+variable "scan_on_push" {
+  description = "푸시 시 이미지 자동 보안 스캔 여부"
+  type        = bool
+  default     = true
+}
+
+variable "encryption_type" {
+  description = "암호화 방식"
+  type        = string
+  default     = "AES256"
+}
