b80e0e402af2d170bfc5008769e717a64afa6508
wcorn <dsk08208@gmail.com>
2025-06-19 14:29:38 +0900

    Fix: dev  환경 수정
    - 잘못 기입된 dev의 환경 수정
     #13

--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -56,7 +56,7 @@ variable "common_tags" {
   description = "모든 리소스에 적용할 공통 태그"
   type        = map(string)
   default = {
-    Environment = "peter"
+    Environment = "dev"
     Owner       = "dolpin"
   }
 }
@@ -64,7 +64,7 @@ variable "common_tags" {
 variable "env" {
   description = "환경"
   type        = string
-  default     = "peter"
+  default     = "dev"
 }
 
 variable "db_engine" {
