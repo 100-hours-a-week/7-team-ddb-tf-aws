3e81ceed5dfa0e7a35f8ec5cf2fd4604bee429bb
Jiyoung Shin <sjy140801@dgu.ac.kr>
2025-06-19 14:36:30 +0900

    Feat: shared-to-prod vpc peering 적용
     #3

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -27,3 +27,16 @@ module "network" {
   common_tags     = var.common_tags
   env             = var.env
 }
+
+module "shared_to_prod_peering" {
+  source = "../../modules/vpc_peering"
+
+  env                        = var.env
+  component                  = "shared-to-prod"
+  requester_vpc_id           = data.terraform_remote_state.shared.outputs.vpc_id
+  accepter_vpc_id            = module.network.vpc_id
+  requester_vpc_cidr         = data.terraform_remote_state.shared.outputs.vpc_cidr
+  accepter_vpc_cidr          = var.vpc_cidr
+  requester_route_table_ids  = data.terraform_remote_state.shared.outputs.private_route_table_ids
+  accepter_route_table_ids   = module.network.private_route_table_ids
+}
