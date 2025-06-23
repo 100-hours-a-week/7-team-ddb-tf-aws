de92b15a05cc8ae0badac3bb93bb979e36895f66
Jiyoung Shin <sjy140801@dgu.ac.kr>
2025-06-23 11:06:19 +0900

    Feat: EC2 인스턴스 리소스 정의 추가
    - AMI ID, 인스턴스 타입, 서브넷 ID, User Data 등 외부 입력값으로 설정
    - 루트 볼륨은 gp3, 암호화 및 삭제 정책 설정
     #9

--- envs/shared/modules/ec2/main.tf
+++ envs/shared/modules/ec2/main.tf
@@ -12,3 +12,29 @@ resource "aws_iam_instance_profile" "this" {
   name = "${var.name}-instance-profile"
   role = aws_iam_role.this.name
 }
+
+resource "aws_instance" "this" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  associate_public_ip_address = false
+  vpc_security_group_ids = [aws_security_group.this.id]
+  iam_instance_profile   = aws_iam_instance_profile.this.name
+  key_name               = null
+
+  root_block_device {
+    volume_size = var.root_volume_size
+    volume_type = "gp3"
+    encrypted   = true
+    delete_on_termination = true
+    tags = {
+      Name = "${var.name}-root-volume"
+    }
+  }
+
+  tags = merge(var.common_tags, {
+    Name = var.name
+  })
+
+  user_data = var.user_data
+}
--- envs/shared/modules/ec2/variables.tf
+++ envs/shared/modules/ec2/variables.tf
@@ -2,3 +2,40 @@ variable "name" {
   description = "이 인스턴스의 이름"
   type        = string
 }
+
+variable "instance_type" {
+  description = "EC2 인스턴스 타입"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "이 인스턴스가 속할 서브넷 ID"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "보안 그룹용 VPC ID"
+  type        = string
+}
+
+variable "user_data" {
+  description = "User data script"
+  type        = string
+  default     = ""
+}
+
+variable "common_tags" {
+  description = "공통 태그"
+  type        = map(string)
+}
+
+variable "root_volume_size" {
+  type        = number
+  description = "Root volume size in GB"
+  default     = 30
+}
