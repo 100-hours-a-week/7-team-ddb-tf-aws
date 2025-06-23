3b13c79f8dc8da602a7e23888c950e8f34756a5f
Jiyoung Shin <sjy140801@dgu.ac.kr>
2025-06-23 14:57:42 +0900

    Feat: EC2별로 IAM Role Policy 적용
    - cicd instance에는 ecr 권한 정책 부여
    - monitoring instance에는 s3 권한 정책 부여
     #9

--- envs/shared/modules/iam/main.tf
+++ envs/shared/modules/iam/main.tf
@@ -8,6 +8,35 @@ resource "aws_iam_role_policy_attachment" "ssm" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
+resource "aws_iam_role_policy_attachment" "ecr" {
+  count      = var.attach_ecr ? 1 : 0
+  role       = aws_iam_role.this.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
+}
+
+resource "aws_iam_policy" "s3_access" {
+  count       = var.attach_s3 ? 1 : 0
+  name        = "${var.role_name}-s3-access"
+  path        = "/"
+  description = "Access to S3 for Thanos/Loki"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect   = "Allow",
+      Action   = ["s3:*"],
+      Resource = [
+        ////
+      ]
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "s3" {
+  count      = var.attach_s3 ? 1 : 0
+  role       = aws_iam_role.this.name
+  policy_arn = aws_iam_policy.s3_access[0].arn
+}
+
 resource "aws_iam_instance_profile" "this" {
   name = "${var.role_name}-profile"
   role = aws_iam_role.this.name
--- envs/shared/modules/iam/variables.tf
+++ envs/shared/modules/iam/variables.tf
@@ -2,3 +2,15 @@ variable "role_name" {
   description = "IAM Role 이름"
   type        = string
 }
+
+variable "attach_ecr" {
+  description = "ECR 권한을 부여할지 여부"
+  type        = bool
+  default     = false
+}
+
+variable "attach_s3" {
+  description = "S3 접근 권한을 부여할지 여부"
+  type        = bool
+  default     = false
+}
