8cc255b9ee351b301505338df94528ed686bf7db
Sj0-0i <sjy140801@dgu.ac.kr>
2025-06-24 22:11:52 +0900

    Feat: CloudFront OAC에 S3 객체 접근 허용 정책 추가
    - CloudFront 서비스 principal에 GetObject 허용
    - SourceArn 조건으로 배포 식별자 제한
    
    #7

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -157,3 +157,32 @@ resource "aws_cloudfront_response_headers_policy" "cors" {
     origin_override = true
   }
 }
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket_policy" "allow_cf" {
+  bucket = aws_s3_bucket.this.id
+  policy = data.aws_iam_policy_document.allow_cf.json
+}
+
+# S3와 CloudFront 연결 위한 리소스
+data "aws_iam_policy_document" "allow_cf" {
+  statement {
+    sid     = "AllowCloudFrontOAC"
+    effect  = "Allow"
+    actions = ["s3:GetObject"]
+
+    resources = ["${aws_s3_bucket.this.arn}/*"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = ["arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.this.id}"]
+    }
+  }
+}
