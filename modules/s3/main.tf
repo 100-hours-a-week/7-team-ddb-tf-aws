76fc6fe1e32033bd960d90aff0c2d0016c2ea3f5
Sj0-0i <sjy140801@dgu.ac.kr>
2025-06-24 22:08:34 +0900

    Feat: CloudFront 캐시, 응답 헤더 정책 추가
    - 캐시 정책에 쿼리스트링, 쿠키, 헤더 설정 포함
    - CORS 허용 도메인, 헤더, 메서드 설정
    
    #7

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -114,3 +114,46 @@ resource "aws_cloudfront_distribution" "this" {
     Name = "cdn-${var.bucket_name}"
   })
 }
+
+resource "aws_cloudfront_cache_policy" "image_cdn_cache" {
+  name = "image-cdn-cache-${var.env}"
+
+  default_ttl = 3600  
+  max_ttl     = 86400 
+  min_ttl     = 0      
+
+  parameters_in_cache_key_and_forwarded_to_origin {
+    cookies_config {  
+      cookie_behavior = "none"
+    }
+
+    headers_config {
+      header_behavior = "whitelist"
+      headers {
+        items = ["Origin"]
+      }
+    }
+
+    query_strings_config { 
+      query_string_behavior = "none"
+    }
+  }
+}
+
+resource "aws_cloudfront_response_headers_policy" "cors" {
+  name = "cors-policy-${var.env}"
+
+  cors_config {
+    access_control_allow_credentials = false
+    access_control_allow_headers {
+      items = ["*"]
+    }
+    access_control_allow_methods {
+      items = ["GET", "HEAD", "OPTIONS"]
+    }
+    access_control_allow_origins {
+      items = var.cors_origins
+    }
+    origin_override = true
+  }
+}
